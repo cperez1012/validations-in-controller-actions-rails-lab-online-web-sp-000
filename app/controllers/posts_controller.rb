f15da272f0fa841a982d1edbbaa30a45ea7b5f05
Christian Perez <perezchristian1012@gmail.com>
2020-01-06 06:43:07 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,11 +9,10 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-      if @post.update(post_params)
-        redirect_to post_path(@post)
-      else
-        render :edit
-      end
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
     end
   end
 
